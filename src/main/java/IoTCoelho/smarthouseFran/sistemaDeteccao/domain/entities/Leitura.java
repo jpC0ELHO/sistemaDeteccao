d73275c474233819ae2a139348815d760da22265
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-27 16:47:04 -0300

    created Leitura

---
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/domain/entities/Leitura.java
@@ -0,0 +1,46 @@
+package IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities;
+
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.enums.DeteccaoTipo;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "leitura")
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Leitura extends Entidade{
+    @Column(nullable = false, name = "deteccao_sim_nao")
+    private Boolean  deteccao_sim_nao;
+    @Enumerated(EnumType.STRING)
+    private DeteccaoTipo deteccaoTipo;
+    @Column(name = "horario_detectado")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
+    @JsonSerialize(using = DateSerializer.class)
+    private LocalDateTime horarioDetec;
+    @ManyToOne
+    @JoinColumn(name = "localizacao_id", nullable = false)
+    private Local localizacao;
+    @ManyToOne
+    @JoinColumn(name = "sensor_id", nullable = false)
+    private Sensores sensor;
+
+}
+
