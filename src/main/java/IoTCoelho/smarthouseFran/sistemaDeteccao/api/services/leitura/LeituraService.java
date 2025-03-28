5835a37c307a1a5e81dc69b16fb0a96acb491af1
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-28 15:26:53 -0300

    created LeituraService

---
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/api/services/leitura/LeituraService.java
@@ -0,0 +1,16 @@
+package IoTCoelho.smarthouseFran.sistemaDeteccao.api.services.leitura;
+
+import IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos.LeituraRequest;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos.LeituraResponse;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface LeituraService {
+    List<LeituraResponse>findLeituraList();
+    Optional<LeituraResponse>findLeituraId(UUID uuid);
+    void createLeitura(LeituraRequest leituraRequest);
+    void updateLeitura(UUID uuid,LeituraRequest leituraRequest);
+    void deleteLeitura(UUID uuid);
+}
