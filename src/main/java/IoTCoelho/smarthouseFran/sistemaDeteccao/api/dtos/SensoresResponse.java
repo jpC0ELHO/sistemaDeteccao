e4078554a1c08611c3cee988e4ea1c1ba6069166
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-28 09:53:22 -0300

    created SensoresResponse

---
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/api/dtos/SensoresResponse.java
@@ -0,0 +1,75 @@
+package IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos;
+
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.Sensores;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.enums.Regiao;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.enums.SensoresTipo;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonPropertyOrder({"uuid","nome","ativadoDesativado","horarioAcionamento","memoriaUsada","memoriaDisponivel",
+"valorDadosTransferencia","dadosDescricao","regiao","sensorTipo","createdBy","lastModifiedBy","createdAt","updatedAt"})
+public record SensoresResponse(
+        UUID uuid,
+        String nome,
+        boolean ativadoDesativado,
+        @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+        LocalDateTime horarioAcionamento,
+        BigDecimal memoriaUsada,
+        BigDecimal memoriaDisponivel,
+        BigDecimal valorDadosTransferencia,
+        String dadosDescricao,
+        Map<Regiao,Boolean>regiao,
+        Map<SensoresTipo,Boolean>sensoresTipo,
+        String createdBy,
+        String lastModifiedBy,
+        @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+        LocalDateTime createdAt,
+        @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+        LocalDateTime updatedAt
+
+
+) {
+    public static SensoresResponse toResponse(Sensores sensores){
+        if (sensores==null){
+            return null;
+        }
+        return new SensoresResponse(
+          sensores.getUuid(),
+          sensores.getNome(),
+          sensores.isAtivadoDesativado(),
+          sensores.getHorarioAcionamento(),
+          sensores.getMemoriaUsada(),
+          sensores.getMemoriaDisponivel(),
+          sensores.getValorDadosTransferencia(),
+          sensores.getDadosDescricao(),
+          sensores.getRegiao(),
+          sensores.getSensorTipo(),
+          sensores.getCreatedBy(),
+          sensores.getLastModifiedBy(),
+          sensores.getCreatedAt(),
+          sensores.getUpdatedAt()
+        );
+    }
+}
