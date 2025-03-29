93cfa51ebdd6b2ca103826484f75a3e9c1a2a082
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-29 17:12:42 -0300

    created LocalizacaoController

---
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/api/controllers/LocalizacaoController.java
@@ -0,0 +1,53 @@
+package IoTCoelho.smarthouseFran.sistemaDeteccao.api.controllers;
+
+import IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos.LocalizacaoRequest;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos.LocalizacaoResponse;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.api.services.localizacao.LocalizacaoService;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.enums.Regiao;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/localizacao/v1")
+@AllArgsConstructor
+public class LocalizacaoController {
+    private final LocalizacaoService localizacaoService;
+
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<LocalizacaoResponse>>findLocalizacaoList(){
+        return ResponseEntity.status(HttpStatus.FOUND).body(localizacaoService.findLocalizacaoList());
+    }
+
+    @GetMapping(value = "/{id}",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Optional<LocalizacaoResponse>>findLocalizacaoId(@PathVariable UUID id){
+        return ResponseEntity.status(HttpStatus.FOUND).body(localizacaoService.findLocalizacaoId(id));
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createLocalizacao(@RequestBody LocalizacaoRequest localizacaoRequest){
+        localizacaoService.createLocalizacao(localizacaoRequest);
+    }
+
+    @PutMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateLocalizacao(@PathVariable UUID id,@RequestBody LocalizacaoRequest localizacaoRequest){
+        localizacaoService.updateLocalizacao(id,localizacaoRequest);
+    }
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteLoocalizacao(@PathVariable UUID id){
+        localizacaoService.deleteLocalizacao(id);
+    }
+
+    @PatchMapping(value = "/{id}/regiao")
+    public void atualizarRegiao(@PathVariable UUID id, @RequestBody Map<Regiao,Boolean>novasRegioes){
+        localizacaoService.atualizarRegiao(id,novasRegioes);
+    }
+}
