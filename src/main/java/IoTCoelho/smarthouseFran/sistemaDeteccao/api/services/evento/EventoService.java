7eb9321031181f99f297d1bbfbfc005d0becb0bf
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-04-02 17:58:16 -0300

    updated EventoService

--- src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/api/services/evento/EventoService.java
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/api/services/evento/EventoService.java
@@ -2,6 +2,9 @@ package IoTCoelho.smarthouseFran.sistemaDeteccao.api.services.evento;
 
 import IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos.EventoRequest;
 import IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos.EventoResponse;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.Evento;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.Leitura;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.enums.EventoTipo;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +14,7 @@ public interface EventoService {
     List<EventoResponse>findEventoList();
     Optional<EventoResponse>findEventoId(UUID uuid);
     void createEvento(EventoRequest eventoRequest);
+    Evento createEventoLeitura(Leitura leitura, EventoTipo eventoTipo);
     void updateEvento(UUID uuid,EventoRequest eventoRequest);
     void deleteEvento(UUID uuid);
 }
