ddd89d89fd78eb27174cd90ae000e59d2ce275cb
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-04-02 19:27:21 -0300

    updated WebSocketController

--- src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/api/controllers/WebSocketController.java
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/api/controllers/WebSocketController.java
@@ -5,6 +5,7 @@ import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.Leitura;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -12,16 +13,33 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @Slf4j
 @CrossOrigin(origins = "*")
 public class WebSocketController {
+    private final SimpMessagingTemplate messagingTemplate;
+    public WebSocketController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
     @MessageMapping("/alerta")
     @SendTo("/topic/alertas")
-    public Evento enviarAlerta(Evento evento){
-        log.info("Alerta disparado: {}",evento);
+    public Evento enviarAlerta(Evento evento) {
+        log.info("Alerta disparado: {}", evento);
         return evento;
     }
+
     @MessageMapping("/afericao")
     @SendTo("/topic/afericao")
-    public Leitura afericao(Leitura leitura){
-        log.info("Aferição de Sensores: {}",leitura);
+    public Leitura afericao(Leitura leitura) {
+        log.info("Aferição de Sensores: {}", leitura);
         return leitura;
+    }
+
+    public void enviarEvento(Evento evento) {
+        messagingTemplate.convertAndSend("/topic/alertas", evento);
+        log.info("Evento enviado via WebSocket: {}", evento);
+    }
+
+
+    public void enviarAfericao(Leitura leitura) {
+        messagingTemplate.convertAndSend("/topic/afericao", leitura);
+        log.info("Aferição enviada via WebSocket: {}", leitura);
     }
 }
