cd8e12006e734cf001a85faab644d87c30ff77c2
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-04-02 17:59:37 -0300

    updated LeituraServiceImp

--- src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/api/services/leitura/LeituraServiceImp.java
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/api/services/leitura/LeituraServiceImp.java
@@ -1,14 +1,22 @@
 package IoTCoelho.smarthouseFran.sistemaDeteccao.api.services.leitura;
 
+import IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos.EventoResponse;
 import IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos.LeituraRequest;
 import IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos.LeituraResponse;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.Evento;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.Leitura;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.enums.EventoTipo;
 import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.exceptions.ModelNotFoundException;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.repositories.EventoRepository;
 import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.repositories.LeituraRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,6 +27,8 @@ import static IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos.LeituraRequest.t
 @AllArgsConstructor
 public class LeituraServiceImp implements LeituraService {
         private final LeituraRepository leituraRepository;
+        private final SimpMessagingTemplate simpMessagingTemplate;
+        private final EventoRepository eventoRepository;
     @Override
     public List<LeituraResponse> findLeituraList() {
         try {
@@ -44,6 +54,26 @@ public class LeituraServiceImp implements LeituraService {
     }
 
     @Override
+    public void processarLeitura(LeituraRequest leituraRequest) {
+        Leitura leitura=leituraRepository.save(toEntidade(leituraRequest));
+        int valor=leituraRequest.valorAferido();
+        Optional<EventoTipo>eventoTipoOpt=EventoTipo.detectarEvento(valor);
+        eventoTipoOpt.ifPresent(eventoTipo->{
+            Evento evento=new Evento();
+            evento.setSensor(leituraRequest.sensores());
+            evento.setHorarioEvento(LocalDateTime.now());
+            evento.setDescricao("Evento detectado: "+eventoTipo.name());
+            evento.setEventoTipo(Map.of(eventoTipo,true));
+            evento.setLocal(leitura.getLocal());
+            Evento eventoSalvo=eventoRepository.save(evento);
+            EventoResponse eventoResponse=EventoResponse.toResponse(eventoSalvo);
+            simpMessagingTemplate.convertAndSend("/topic/alertas",eventoResponse);
+            log.info("Evento detectado enviado: {}",eventoResponse);
+        });
+    }
+
+
+    @Override
     public void createLeitura(LeituraRequest leituraRequest) {
         //Armazenar qualquer tipo de leitura, por isso não há tratamento, isso será gerado de forma automatica
        leituraRepository.save(toEntidade(leituraRequest));
@@ -54,7 +84,7 @@ public class LeituraServiceImp implements LeituraService {
     var findLeituraId=leituraRepository
             .findById(uuid)
             .orElseThrow(()-> new ModelNotFoundException("ID: "+uuid+" not found!"));
-    findLeituraId.setDeteccao_sim_nao(leituraRequest.deteccao_sim_nao());
+    findLeituraId.setDeteccaoSimNao(leituraRequest.deteccao_sim_nao());
     findLeituraId.setDeteccaoTipo(leituraRequest.deteccaoTipo());
     findLeituraId.setSensor(leituraRequest.sensores());
     findLeituraId.setLocal(leituraRequest.local());
@@ -67,6 +97,7 @@ public class LeituraServiceImp implements LeituraService {
         var findLeituraId=leituraRepository
                 .findById(uuid)
                 .orElseThrow(()-> new ModelNotFoundException("ID : "+uuid+" not found!"));
+        leituraRepository.delete(findLeituraId);
     }catch (RuntimeException e){
         log.error("Erro:{}",e.getMessage());
         throw e;
