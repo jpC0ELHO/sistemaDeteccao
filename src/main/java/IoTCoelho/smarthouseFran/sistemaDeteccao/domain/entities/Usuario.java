ddefa645e09080b6ccbe922261b99de72c8f8cdb
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-04-17 22:24:46 -0300

    update Usuario

--- src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/domain/entities/Usuario.java
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/domain/entities/Usuario.java
@@ -3,18 +3,20 @@ package IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities;
 import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.enums.Role;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+
 
 @Entity
 @Data
@@ -23,20 +25,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class Usuario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID uuid;
-
-    @CreatedDate
-    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
-    private LocalDateTime updatedAt;
+public class Usuario extends Entidade {
 
     @Column(unique = true, nullable = false)
     private String username;
@@ -53,4 +42,10 @@ public class Usuario {
 
     @Column(name = "ativo")
     private Boolean ativo = true;
+
+    @JsonSerialize(using = DateSerializer.class)
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd/MM/yyyy")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private LocalDateTime dataNasci;
 }
