81486509ae203e25fa1290e1e17911bfc8546a0b
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-04-17 22:22:57 -0300

    created UsuarioResponse

---
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/api/dtos/UsuarioResponse.java
@@ -0,0 +1,59 @@
+package IoTCoelho.smarthouseFran.sistemaDeteccao.api.dtos;
+
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.Usuario;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.domain.entities.enums.Role;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonPropertyOrder({"uuid","usarname","password","email","role","ativo","createdBy","lastModifiedBy","createdAt","updatedAt"})
+public record UsuarioResponse(
+        UUID uuid,
+        String username,
+        String password,
+        String email,
+        Role role,
+        Boolean ativo,
+        String createdBy,
+        String lastModifiedBy,
+        @DateTimeFormat(pattern = "dd/MM/yyyy")
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
+        @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
+        @JsonSerialize(using = DateSerializer.class)
+        LocalDateTime createdAt,
+        @DateTimeFormat(pattern = "dd/MM/yyyy")
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
+        @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
+        @JsonSerialize(using = DateSerializer.class)
+        LocalDateTime updateAt
+
+) {
+    public static UsuarioResponse toResponse(Usuario usuario){
+        if (usuario==null){
+            return null;
+        }
+        return new UsuarioResponse(
+                usuario.getUuid(),
+                usuario.getUsername(),
+                usuario.getPassword(),
+                usuario.getEmail(),
+                usuario.getRole(),
+                usuario.getAtivo(),
+                usuario.getCreatedBy(),
+                usuario.getLastModifiedBy(),
+                usuario.getCreatedAt(),
+                usuario.getUpdatedAt()
+
+        );
+    }
+}
