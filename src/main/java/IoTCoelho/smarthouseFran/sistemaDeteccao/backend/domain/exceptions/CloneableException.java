629378000292c0ee84b2254770680e46eb3bf7aa
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-11-20 15:42:52 -0300

    created CloneableException

---
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/backend/domain/exceptions/CloneableException.java
@@ -0,0 +1,7 @@
+package IoTCoelho.smarthouseFran.sistemaDeteccao.backend.domain.exceptions;
+
+public class CloneableException extends CloneNotSupportedException{
+    public CloneableException(String message){
+        super(message);
+    }
+}
