979e164b0eb833180690d1b50fb89fe390ad37d4
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-17 09:14:14 -0300

    created DeviceRepository

---
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/backend/domain/repositories/DeviceRepository.java
@@ -0,0 +1,11 @@
+package IoTCoelho.smarthouseFran.sistemaDeteccao.backend.domain.repositories;
+
+import IoTCoelho.smarthouseFran.sistemaDeteccao.backend.domain.entities.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface DeviceRepository extends JpaRepository<Device, UUID> {
+    Optional<Device>findById(UUID uuid);
+}
