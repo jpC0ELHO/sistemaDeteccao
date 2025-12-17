1674579ea457fcbaaa2f13b8da6667ed70c3e4d8
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-17 09:19:29 -0300

    updated UsuarioService

--- src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/backend/api/services/usuario/UsuarioService.java
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/backend/api/services/usuario/UsuarioService.java
@@ -2,6 +2,7 @@ package IoTCoelho.smarthouseFran.sistemaDeteccao.backend.api.services.usuario;
 
 import IoTCoelho.smarthouseFran.sistemaDeteccao.backend.api.dtos.UsuarioRequest;
 import IoTCoelho.smarthouseFran.sistemaDeteccao.backend.api.dtos.UsuarioResponse;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +14,6 @@ public interface UsuarioService {
     void createUsuario(UsuarioRequest usuarioRequest);
     void updateUsuario(UUID uuid,UsuarioRequest usuarioRequest);
     void deleteUsuario(UUID uuid);
+    //user details
+    public UserDetails loadUserByUsername(String username);
 }
