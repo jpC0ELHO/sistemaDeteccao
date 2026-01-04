94b7736cc9cd3553a03647af4d7e1b3e435afeb1
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-01-04 18:28:10 -0300

    created ComunicacaoBrevidadeService

---
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/backend/api/services/comunicacaoBrevidade/ComunicacaoBrevidadeService.java
@@ -0,0 +1,16 @@
+package IoTCoelho.smarthouseFran.sistemaDeteccao.backend.api.services.comunicacaoBrevidade;
+
+import IoTCoelho.smarthouseFran.sistemaDeteccao.backend.api.dtos.ComunicacaoBrevidadeRequest;
+import IoTCoelho.smarthouseFran.sistemaDeteccao.backend.api.dtos.ComunicacaoBrevidadeResponse;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ComunicacaoBrevidadeService {
+    List<ComunicacaoBrevidadeResponse>findComunicacaoList();
+    Optional<ComunicacaoBrevidadeResponse>findComunicacaoId(UUID uuid);
+    void createComunicacaoBrevidade(ComunicacaoBrevidadeRequest comunicacaoBrevidadeRequest);
+    void updateComunicacaoBrevidade(UUID uuid,ComunicacaoBrevidadeRequest comunicacaoBrevidadeRequest);
+    void deleteComunicacaoBrevidade(UUID uuid);
+}
