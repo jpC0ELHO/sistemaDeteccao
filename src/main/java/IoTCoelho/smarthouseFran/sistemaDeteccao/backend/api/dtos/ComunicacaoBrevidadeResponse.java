e031a1004f4ddbf8906f63ef3a1286b2c647310b
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-01-04 18:27:58 -0300

    created ComunicacaoBrevidadeResponse

---
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/backend/api/dtos/ComunicacaoBrevidadeResponse.java
@@ -0,0 +1,79 @@
+package IoTCoelho.smarthouseFran.sistemaDeteccao.backend.api.dtos;
+
+import IoTCoelho.smarthouseFran.sistemaDeteccao.backend.domain.entities.ComunicacaoBrevidade;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JsonPropertyOrder({
+        "uuid",
+        "createdBy",
+        "lastModifiedBy",
+        "createdAt",
+        "updatedAt",
+        "macAddress",
+        "latitudeAtual",
+        "longitudeAtual",
+        "latitudeReferencia",
+        "longitudeReferencia",
+        "distanciaCalculadaKm",
+        "foraDoPerimetro",
+        "brevidadeConfirmada",
+        "enviadoAoSmartwatch",
+        "dataEnvio"
+})
+public record ComunicacaoBrevidadeResponse(
+        UUID uuid,
+        String createdBy,
+        String lastModifiedBy,
+        @DateTimeFormat(pattern = "dd/MM/yyyy")
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
+        @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
+        @JsonSerialize(using = DateSerializer.class)
+        LocalDateTime createdAt,
+        @DateTimeFormat(pattern = "dd/MM/yyyy")
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
+        @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
+        @JsonSerialize(using = DateSerializer.class)
+        LocalDateTime updatedAt,
+        String macAddress,
+        Double latitudeAtual,
+        Double longitudeAtual,
+        Double latitudeReferencia,
+        Double longitudeReferencia,
+        Double distanciaCalculadaKm,
+        Boolean foraDoPerimetro,
+        Boolean brevidadeConfirmada,
+        Boolean enviadoAoSmartwatch,
+        LocalDateTime dataEnvio
+) {
+    public static ComunicacaoBrevidadeResponse toResponse(ComunicacaoBrevidade comunicacaoBrevidade){
+        if (comunicacaoBrevidade==null){
+            return null;
+        }
+        return new ComunicacaoBrevidadeResponse(
+                comunicacaoBrevidade.getUuid(),
+                comunicacaoBrevidade.getCreatedBy(),
+                comunicacaoBrevidade.getLastModifiedBy(),
+                comunicacaoBrevidade.getCreatedAt(),
+                comunicacaoBrevidade.getUpdatedAt(),
+                comunicacaoBrevidade.getMacAddress(),
+                comunicacaoBrevidade.getLatitudeAtual(),
+                comunicacaoBrevidade.getLongitudeAtual(),
+                comunicacaoBrevidade.getLatitudeReferencia(),
+                comunicacaoBrevidade.getLongitudeReferencia(),
+                comunicacaoBrevidade.getDistanciaCalculadaKm(),
+                comunicacaoBrevidade.getForaDoPerimetro(),
+                comunicacaoBrevidade.getBrevidadeConfirmada(),
+                comunicacaoBrevidade.getEnviadoAoSmartwatch(),
+                comunicacaoBrevidade.getDataEnvio()
+        );
+    }
+}
