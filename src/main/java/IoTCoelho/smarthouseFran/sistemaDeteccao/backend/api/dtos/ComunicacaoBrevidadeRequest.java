6e427ea48fabe5850729eccb9d8ac6150507ee02
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-01-04 18:27:42 -0300

    created ComunicacaoBrevidadeRequest

---
+++ src/main/java/IoTCoelho/smarthouseFran/sistemaDeteccao/backend/api/dtos/ComunicacaoBrevidadeRequest.java
@@ -0,0 +1,41 @@
+package IoTCoelho.smarthouseFran.sistemaDeteccao.backend.api.dtos;
+
+import IoTCoelho.smarthouseFran.sistemaDeteccao.backend.domain.entities.ComunicacaoBrevidade;
+
+import java.time.LocalDateTime;
+
+public record ComunicacaoBrevidadeRequest(
+        String macAddress,
+
+        Double latitudeAtual,
+        Double longitudeAtual,
+
+        Double latitudeReferencia,
+        Double longitudeReferencia,
+
+        Double distanciaCalculadaKm,
+
+        Boolean foraDoPerimetro,
+        Boolean brevidadeConfirmada,
+
+        Boolean enviadoAoSmartwatch,
+        LocalDateTime dataEnvio
+){
+    public static ComunicacaoBrevidade toEntidade(ComunicacaoBrevidadeRequest comunicacaoBrevidadeRequest){
+        if (comunicacaoBrevidadeRequest==null){
+            return null;
+        }
+        return new ComunicacaoBrevidade(
+                comunicacaoBrevidadeRequest.macAddress(),
+                comunicacaoBrevidadeRequest.latitudeAtual(),
+                comunicacaoBrevidadeRequest.longitudeAtual(),
+                comunicacaoBrevidadeRequest.latitudeReferencia(),
+                comunicacaoBrevidadeRequest.longitudeReferencia(),
+                comunicacaoBrevidadeRequest.distanciaCalculadaKm(),
+                comunicacaoBrevidadeRequest.foraDoPerimetro(),
+                comunicacaoBrevidadeRequest.brevidadeConfirmada(),
+                comunicacaoBrevidadeRequest.enviadoAoSmartwatch(),
+                comunicacaoBrevidadeRequest.dataEnvio()
+        );
+    }
+}
